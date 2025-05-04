53da2b99687762dc1c2b8159356fd625c417828c
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-05-04 21:33:53 +0400

    modified:   demo.py

--- demo.py
+++ demo.py
@@ -3,5 +3,14 @@ import time
 import mlflow
 
 def eval(param_1, param_2):
-    return param_1 + param_2
+    return (param_1 + param_2)/2
+
+
+def mani():
+    with mlflow.start_run as run:
+        mlflow.log_param('param_1', param_1)
+        mlflow.log_param('param_2', param_2)
+        mlflow.log_metric('mean', eval(param_1=param_1, param_2=param_2))
+        
+
     
