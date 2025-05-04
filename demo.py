89b08961e47a0ca4c889c9a625d83c53059ae662
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-05-04 22:59:49 +0400

    modified:   demo.py 	modified:   mlruns/0/01fd7a1999b34c57a0b2fa48542bdb79/meta.yaml 	modified:   mlruns/0/1cb9ffa666d846458dec1cc6fb0b75ba/meta.yaml 	modified:   mlruns/0/bfe5de8b4a7045a3bf8581f43faff6ef/meta.yaml

--- demo.py
+++ demo.py
@@ -1,33 +1,3 @@
-# import os
-# import time
-# import mlflow
-# import argparse
-
-# def eval(param_1, param_2):
-#     return (param_1 + param_2)/2
-
-
-# def main(param_1, param_2):
-
-#     with mlflow.start_run():
-#         mlflow.set_tag('version', '1.0.0')
-#         mlflow.log_param('param_1', param_1)
-#         mlflow.log_param('param_2', param_2)
-#         mlflow.log_metric('mean', eval(param_1=param_1, param_2=param_2))
-
-
-
-    
-# if __name__ == '__main__':
-
-#     parser = argparse.ArgumentParser(prog='myprogram')
-#     parser.add_argument('--param_1', '-p1', default=10, type=int)
-#     parser.add_argument('--param_2', '-p2', default=20, type=int)
-#     args = parser.parse_args()
-
-#     main(param_1=args.param_1, param_2=args.param_2)
-
-
 import os
 import mlflow
 import argparse
