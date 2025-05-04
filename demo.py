8ca537c0d4d565ce70ed081f5c9d0fef5a4faa53
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-05-04 22:48:13 +0400

    .

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ def eval(param_1, param_2):
     return (param_1 + param_2)/2
 
 
-def mani(param_1, param_2):
+def main(param_1, param_2):
 
     with mlflow.start_run as run:
         mlflow.set_tag('version', '1.0.0')
